58eda8a0773e72505b122f619694c931bb551bc4
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-06-25 00:06:22 +0300

    PromptAlertConfirm

---
+++ AlertPromtConfirm/index.js
@@ -0,0 +1,25 @@
+// аля отладочные инструменты
+
+alert("hello"); 
+
+let title = "Card number"
+let cardNumber =  prompt(title, null); // prompt нужен для получени инфы от юзверя
+// null d prompt указан как дефолтное значение 
+
+// инструкция confirm :
+let  flag = confirm("How old are you?") // будет вопрос ok - cancel \
+// если ответ был ok - то в Flag лежит true, иначе false
+
+
+
+// task
+let Name = prompt("Write your first name:");
+alert(Name);
+
+
+
+
+
+
+
+
