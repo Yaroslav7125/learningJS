8ba075e901043553a68810c147668e8b0c21f6cd
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-06-24 19:42:53 +0300

    Add types

---
+++ Types/index.js
@@ -0,0 +1,15 @@
+let a = 123;
+let b = 123n; // biInt
+ let c = typeOf(Symbol("lsbdjalksmdl")); 
+alert(`bigInt is ${typeof(b)}` + " " + `number is ${typeof(a)}`);
+
+let CheckedFlag = true;
+let UnCheced = false;
+
+
+//
+/*
+1) hello 1
+2)hello name
+3) Hello Ilya
+*/
