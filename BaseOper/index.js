7d0367567a304ca82561601c2b7d8f08a9ac7bcf
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-06-26 03:03:47 +0300

    Add Base Oper

---
+++ BaseOper/index.js
@@ -0,0 +1,48 @@
+let a = prompt("Wirte a:")
+let b = prompt("Wirte b:")
+
+
+alert(`a + b ` + `${Number(b) + Number(a)}`);
+alert(`a ^ b ` + `${a**b}`);
+alert(`a - b ` + `${a-b}`);
+alert(`a* b ` + `${a*b}`);
+alert(`a / b ` + `${a/b}`); // деление
+alert(`a % b ` + `${a%b}`); // деление с остатком
+
+
+//tasks
+/*
+1) c = 2
+   d = 1
+   a = 2 
+   b = 2
+
+2) 5
+
+*/
+
+/*
+3)
+"10"
+-1
+1
+2
+6
+9px
+$45
+infinity"
+" -9 5"
+-14
+1 + null = 1
+1 + underfinded = Nan
+
+
+
+
+let a = Number(prompt("Введите первое число",1)); 
+let b = Number(prompt("Введите первое число",1)); 
+
+alert(a+b);
+*/
+
+
