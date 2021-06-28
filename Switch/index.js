e99a2a110c1530d15fdeb4a5379295489f0425a1
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-06-28 19:56:29 +0300

    Add Switch

--- Switch/index.js
+++ Switch/index.js
@@ -10,3 +10,19 @@ if (browser == "Edge") {
 } else {
     alert("So, We hope that this page looks OK!")
 }
+
+/**/
+
+let number = 0;
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0')
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
+}
